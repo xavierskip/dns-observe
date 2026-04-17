d10d163e189be6cacbc505f4a8d041753b2a9824
xavier skip <xavierskip@gmail.com>
2026-04-18 00:18:42 +0800

    添加用法示例，展示 DNS 查询及响应打印功能

---
+++ useage.py
@@ -0,0 +1,24 @@
+from dns_observe import DNSQuery, RecordType, DNSResponse
+
+def print_responses(responses: list[DNSResponse]):
+    for res in responses:
+        print(f'{res}')
+        for ans in res.answers:
+            print(f'└ {ans}')
+    
+    print('---\n')
+
+dns = DNSQuery('1.1.1.1', wait_time=3)
+
+responses: list[DNSResponse] = dns.query("api.openai.com")
+print_responses(responses)
+
+
+# custom transaction ID for dns query, useful for tracking specific queries in logs or network captures
+dns.transaction_id = 0x666
+responses = dns.query('vscode-sync.trafficmanager.net', RecordType.AAAA)
+print_responses(responses)
+
+dns.transaction_id = 0x1234
+responses = dns.query('twitter.com', RecordType.CNAME)
+print_responses(responses)
